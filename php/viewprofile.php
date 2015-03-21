246bca09d5e25e5dcd60f761454d8f0e9c20282c
moonblade <moonblade168@gmail.com>
2015-03-22 04:35:43 +0530

    viewprofile edits

--- php/viewprofile.php
+++ php/viewprofile.php
@@ -1,15 +1,15 @@
 <?PHP
 $email = $_REQUEST['email'];
-$pass = md5($_REQUEST['password']);
+$pass = $_REQUEST['password'];
 $mode=0;
-if (isset($_REQUEST['bloodgroup'])) {
-    $bloodgroup=$_REQUEST['bloodgroup'];
-    $mode=$mode+1;
-}
-if (isset($_REQUEST['branch'])) {
-    $branch=$_REQUEST['branch'];
-    $mode=$mode+2;
-}
+// if (isset($_REQUEST['bloodgroup'])) {
+//     $bloodgroup=$_REQUEST['bloodgroup'];
+//     $mode=$mode+1;
+// }
+// if (isset($_REQUEST['branch'])) {
+//     $branch=$_REQUEST['branch'];
+//     $mode=$mode+2;
+// }
 include "db_details.php";
 mysql_connect($server, $user_name, $password);
 
@@ -21,18 +21,18 @@ $json['status']=0;
 if ($db_found) {
 	if($mode==0)
 	    $SQL = "SELECT name,email,bloodgroup,branch,mobile,year FROM $tablename WHERE email='$email' and password='$pass' ";
-    elseif($mode==1)
-	    $SQL = "SELECT name,email,bloodgroup,branch,mobile,year FROM $tablename WHERE email='$email' and password='$pass' and  bloodgroup='$bloodgroup'";
-    elseif($mode==2)
-	    $SQL = "SELECT name,email,bloodgroup,branch,mobile,year FROM $tablename WHERE email='$email' and password='$pass' and branch='$branch'";
-    elseif($mode==3)
-	    $SQL = "SELECT name,email,bloodgroup,branch,mobile,year FROM $tablename WHERE email='$email' and password='$pass' and branch='$branch' and bloodgroup='$bloodgroup'";
+    // elseif($mode==1)
+	   //  $SQL = "SELECT name,email,bloodgroup,branch,mobile,year FROM $tablename WHERE email='$email' and password='$pass' and  bloodgroup='$bloodgroup'";
+    // elseif($mode==2)
+	   //  $SQL = "SELECT name,email,bloodgroup,branch,mobile,year FROM $tablename WHERE email='$email' and password='$pass' and branch='$branch'";
+    // elseif($mode==3)
+	   //  $SQL = "SELECT name,email,bloodgroup,branch,mobile,year FROM $tablename WHERE email='$email' and password='$pass' and branch='$branch' and bloodgroup='$bloodgroup'";
     $result = mysql_query($SQL) or die($json['error']=mysql_error());
     $num_rows = mysql_num_rows($result);
     if ($num_rows) {
     	$json['status']=1;
         while ($row=mysql_fetch_assoc($result)) {
-            $json['donor_info'][]=$row;
+            $json['donor_info']=$row;
         }
     }
 
