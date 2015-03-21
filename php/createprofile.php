6f8848eaa46c5b87ba1fac0e4988adce0c82619e
moonblade <moonblade168@gmail.com>
2015-03-21 20:51:28 +0530

    create profile done

---
+++ php/createprofile.php
@@ -0,0 +1,39 @@
+<?PHP
+$email = $_REQUEST['email'];
+$pass = md5($_REQUEST['password']);
+$bloodgroup = $_REQUEST['bloodgroup'];
+$bloodgroup = $_REQUEST['bloodgroup'];
+$branch = $_REQUEST['branch'];
+$dt=0;
+if (isset($_REQUEST['date'])) {
+	$dt=1;
+	$date = $_REQUEST['date'];
+}
+$year_of_passing = $_REQUEST['year'];
+$iyear=intval($year_of_passing);
+if($dt)
+	$idate=intval($date);
+include("db_details.php");
+mysql_connect($server, $user_name, $password);
+$db_handle = mysql_connect($server, $user_name, $password);
+$db_found = mysql_select_db($database, $db_handle);
+$json=array();
+$json['status']=0;
+if ($db_found) {
+	$check_exist="SELECT * from $tablename where email='$email' and password='$pass'";
+	if (!mysql_num_rows(mysql_query($check_exist))) {
+		$json['error']="No user Found, Please Login";
+	} else {
+		if($dt)
+			$SQL=" UPDATE $tablename SET bloodgroup='$bloodgroup', branch='$branch', year='$iyear' ,date='$idate' WHERE email='$email' and password='$pass'";
+		else
+			$SQL=" UPDATE $tablename SET bloodgroup='$bloodgroup', branch='$branch', year='$iyear' WHERE email='$email' and password='$pass'";
+		$result = mysql_query($SQL);
+		if ($result) {
+			$json['status']=1;
+		} else {
+			$json['error']=mysql_error();
+		}
+	}
+}
+echo json_encode($json);
---
+++ php/login.php
@@ -0,0 +1,20 @@
+<?PHP
+$email = $_REQUEST['email'];
+$pass = md5($_REQUEST['password']);
+include "db_details.php";
+mysql_connect($server, $user_name, $password);
+$db_handle = mysql_connect($server, $user_name, $password);
+$db_found = mysql_select_db($database, $db_handle);
+$json = array();
+$json['status']=0;
+if ($db_found) {
+    $check_exist="SELECT * from $tablename where email='$email' and password='$pass'";
+    if (!mysql_num_rows(mysql_query($check_exist))) {
+                $json['error']="No user Found";
+    } else {
+        $json['status']=1;
+    }
+} else {
+        $json['error']="Databse not found";
+}
+echo json_encode($json);
